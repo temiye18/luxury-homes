c32197c4af561a625efd1da6a9ea85d9abbb38ae
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2022-07-01 17:06:43 +0100

    little update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "../UI/Container";
 import classes from "./Hero.module.css";
 import videoImg from "../../assets/video.png";
+import vectorLogo from "../../assets/Underline.png";
 
 const Hero = () => {
   return (
@@ -9,10 +10,15 @@ const Hero = () => {
       <Container>
         <div className={classes.welcome__text}>
           <h2>
-            It's not just Real Estate. It's an <br></br> Investment in your
-            Future.
+            It's not just Real Estate. It's an <br /> Investment in your Future.
           </h2>
           <p>The future is here. And we're excited to be part of it</p>
+
+          <img
+            className={classes.vector__logo}
+            src={vectorLogo}
+            alt="vector logo"
+          />
         </div>
 
         <div className={classes.video__container}>
