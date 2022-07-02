c6dca1bc8bbb6151a86eced34e68df02dd014fda
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2022-07-02 08:23:41 +0100

    estate section completed

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,15 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../UI/Container";
+import "animate.css";
 import classes from "./Properties.module.css";
 import vector2 from "../../assets/Vector 2.png";
+import { estate, blog } from "../../data";
 
 const Properties = () => {
+  //   const [myEstate, setMyEstate] = useState(estate);
+  const [value, setValue] = useState(2);
+
+  const { estates } = estate[value];
+
   return (
     <section className={classes.property}>
       <Container>
-        <div className={classes.property__heading}>
-          <h2>Discover & Find Your Dream Property</h2>
-          <img src={vector2} alt="border_bottom" />
+        <div className={classes.property__items}>
+          <div className={classes.property__heading}>
+            <h2>Discover & Find Your Dream Property</h2>
+            <img src={vector2} alt="border_bottom" />
+          </div>
+
+          <div className={classes.states}>
+            {estate.map((state, index) => {
+              return (
+                <button
+                  key={state.id}
+                  onClick={() => setValue(index)}
+                  className={index === value && classes.active}
+                >
+                  {state.state}
+                </button>
+              );
+            })}
+          </div>
+
+          <div
+            className={`${classes.estate} animate__animated animate__slideInRight`}
+          >
+            {estates.map((estate, index) => {
+              const { id, image, name, location, size, deposit, price } =
+                estate;
+              return (
+                <div className={classes.estate__item} key={id}>
+                  <img src={image} alt="estate_pic" />
+                  <div className={classes.estate__info}>
+                    <p className={classes.estate__name}>{name}</p>
+                    <p className={classes.estate__location}>
+                      {location} <span>.</span>
+                      <span style={{ fontSize: "13.82px", color: "#848484" }}>
+                        Size: {size}
+                      </span>
+                    </p>
+                    <p className={classes.estate__deposit}>
+                      Initial Deposit: {deposit}
+                    </p>
+                    <p className={classes.estate__price}>N{price}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </Container>
     </section>
--- src/data.js
+++ src/data.js
@@ -1,6 +1,12 @@
-import panda1 from "./assets/panda_estate_1.jpg";
-import panda2 from "./assets/panda_estate_2.jpg";
-import panda3 from "./assets/panda_estate_3.jpg";
+import panda1 from "./assets/panda_estate_1.png";
+import panda2 from "./assets/panda_estate_2.png";
+import panda3 from "./assets/panda_estate_3.png";
+import panda_oyo1 from "./assets/panda_oyo1.png";
+import panda_oyo2 from "./assets/panda_oyo2.png";
+import panda_oyo3 from "./assets/panda_oyo3.png";
+import panda_ogun1 from "./assets/panda_ogun1.png";
+import panda_ogun2 from "./assets/panda_ogun2.png";
+import panda_ogun3 from "./assets/panda_ogun3.png";
 import blog1 from "./assets/blog_img_1.png";
 import blog2 from "./assets/blog_img_2.png";
 import blog3 from "./assets/blog_img_3.png";
@@ -10,7 +16,7 @@ import blog5 from "./assets/blog_img_5.png";
 export const estate = [
   {
     id: "1",
-    sate: "Lagos State",
+    state: "Lagos State",
     estates: [
       {
         id: "600",
@@ -19,6 +25,7 @@ export const estate = [
         location: "Ibadan, Moniya",
         size: "600 SQM",
         deposit: "N150, 000",
+        price: "750, 000",
       },
       {
         id: "700",
@@ -27,6 +34,7 @@ export const estate = [
         location: "Ibadan Moniya",
         size: "700 SQM",
         deposit: "N150, 000",
+        price: "750, 000",
       },
       {
         id: "800",
@@ -35,68 +43,75 @@ export const estate = [
         location: "Ibadan Moniya",
         size: "800 SQM",
         deposit: "N150, 000",
+        price: "750, 000",
       },
     ],
   },
 
   {
     id: "2",
-    sate: "Oyo State",
+    state: "Oyo State",
     estates: [
       {
         id: "900",
-        image: panda1,
+        image: panda_oyo1,
         name: "Panda Estate",
         location: "Ibadan, Moniya",
         size: "900 SQM",
         deposit: "N150, 000",
+        price: "750, 000",
       },
       {
         id: "910",
-        image: panda2,
+        image: panda_oyo2,
         name: "Panda Estate",
         location: "Ibadan Moniya",
         size: "910 SQM",
         deposit: "N150, 000",
+        price: "750, 000",
       },
       {
         id: "820",
-        image: panda3,
+        image: panda_oyo3,
         name: "Panda Estate",
         location: "Ibadan Moniya",
         size: "820 SQM",
         deposit: "N150, 000",
+        price: "750, 000",
       },
     ],
   },
 
   {
     id: "3",
-    sate: "Ogun State",
+    state: "Ogun State",
     estates: [
       {
         id: "100",
-        image: panda1,
+        image: panda_ogun1,
         name: "Panda Estate",
         location: "Ibadan, Moniya",
         size: "100 SQM",
         deposit: "N150, 000",
+        price: "750, 000",
       },
       {
         id: "80",
-        image: panda2,
+        image: panda_ogun2,
         name: "Panda Estate",
         location: "Ibadan Moniya",
         size: "80 SQM",
         deposit: "N150, 000",
+        price: "750, 000",
       },
       {
         id: "70",
-        image: panda3,
+        image: panda_ogun3,
         name: "Panda Estate",
         location: "Ibadan Moniya",
         size: "70 SQM",
         deposit: "N150, 000",
+        price: "750, 000",
       },
     ],
   },
